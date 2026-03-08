0ff4d8c8edc1e55d64a46f3810d0d0375192adda
scott <scott@SCOTT-REMOTE>
2026-03-08 13:32:23 +1000

    Add project files.

---
+++ NbnStock.Core/Models/StockItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NbnStock.Core.Models
+{
+    public class StockItem
+    {
+        public int Id { get; set; }
+        public string ItemCode { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Quantity { get; set; }
+        public string Unit { get; set; }
+        public int MinimumStock { get; set; }
+        public bool IsSerialised { get; set; }
+        public string Notes { get; set; }
+        public DateTime LastUpdatedUTC { get; set; } = DateTime.UtcNow;
+        
+    }
+}
