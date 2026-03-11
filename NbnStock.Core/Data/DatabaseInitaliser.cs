5e65e64315d7c6f33c212912367a790c2d67dc43
scott dare <scott.dare@outlook.com.au>
2026-03-11 12:00:54 +1000

    Add Sqlite dependency and database initializer class
    Added Microsoft.Data.Sqlite package to enable SQLite support. Introduced DatabaseInitaliser class to set up local app data directory for future database storage. No database file is created yet.

---
+++ NbnStock.Core/Data/DatabaseInitaliser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+using System.IO;
+
+namespace NbnStock.Core.Data
+{
+    public static class DatabaseInitaliser
+    {
+        public static void Initialise()
+        {
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string appFolderPath = System.IO.Path.Combine(appDataPath, "NbnStock");
+            string databaseFilePath = System.IO.Path.Combine(appFolderPath, "NbnStock.db");
+           
+            if (!Directory.Exists(appFolderPath))
+            {
+                Directory.CreateDirectory(appFolderPath);
+            }
+        }
+    }
+}
