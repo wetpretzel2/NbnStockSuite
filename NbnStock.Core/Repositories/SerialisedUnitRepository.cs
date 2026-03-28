cf79630b85e4cbe3129cc0fb8d3fa6b3c8f9018d
scott dare <scott.dare@outlook.com.au>
2026-03-28 16:46:34 +1000

    Add SerialisedUnitRepository class placeholder
    Created SerialisedUnitRepository.cs with necessary using directives and an empty class in the NbnStock.Core.Repositories namespace as a foundation for future development.

---
+++ NbnStock.Core/Repositories/SerialisedUnitRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+using NbnStock.Core.Models;
+using NbnStock.Core.Data;
+
+namespace NbnStock.Core.Repositories
+{
+    public class SerialisedUnitRepository
+    {
+    }
+}
