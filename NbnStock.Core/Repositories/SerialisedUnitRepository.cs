fd4c16ae5eec16f9f892b5370325efd915da9fb2
scott dare <scott.dare@outlook.com.au>
2026-03-28 16:54:56 +1000

    Add AddSerialisedUnit method to repository
    Implemented AddSerialisedUnit in SerialisedUnitRepository to insert new SerialisedUnit records into the database using a parameterized SQL statement and SQLite connection.

--- NbnStock.Core/Repositories/SerialisedUnitRepository.cs
+++ NbnStock.Core/Repositories/SerialisedUnitRepository.cs
@@ -7,5 +7,32 @@ namespace NbnStock.Core.Repositories
 {
     public class SerialisedUnitRepository
     {
+        public void AddSerialisedUnit(SerialisedUnit unit)
+        {
+            string connectionString = $"Data Source={DatabaseInitialiser.DatabasePath}";
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = @"
+            INSERT INTO SerialisedUnits
+            (StockItemId, SerialNumber, Status, Notes, LastUpdatedUtc)
+            VALUES
+            (@StockItemId, @SerialNumber, @Status, @Notes, @LastUpdatedUtc);
+        ";
+
+                using (var command = new SqliteCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@StockItemId", unit.StockItemId);
+                    command.Parameters.AddWithValue("@SerialNumber", unit.SerialNumber);
+                    command.Parameters.AddWithValue("@Status", unit.Status);
+                    command.Parameters.AddWithValue("@Notes", unit.Notes ?? "");
+                    command.Parameters.AddWithValue("@LastUpdatedUtc", unit.LastUpdatedUtc.ToString("o"));
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
