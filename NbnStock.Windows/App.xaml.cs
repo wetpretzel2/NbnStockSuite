0d049c5db2732298a3f87b57521d3b334d8be632
scott dare <scott.dare@outlook.com.au>
2026-04-19 18:56:41 +1000

    Refactor startup and add stock items DataGrid UI
    Removed test/demo logic from App startup, leaving only database initialization. Redesigned MainWindow with a DataGrid to display stock items, including columns for item code, name, quantity, supply type, and serialisation. Added logic to load and bind stock items to the DataGrid on window load. Cleaned up unused usings in MainWindow.xaml.cs.

--- NbnStock.Windows/App.xaml.cs
+++ NbnStock.Windows/App.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using NbnStock.Core.Data;
-using NbnStock.Core.Models;
-using NbnStock.Core.Repositories;
 using System.Windows;
 
 namespace NbnStock.Windows
@@ -12,32 +10,6 @@ namespace NbnStock.Windows
             base.OnStartup(e);
 
             DatabaseInitialiser.Initialise();
-
-            var stockRepo = new StockRepository();
-
-            var allItems = stockRepo.GetAllStockItems();
-
-            if (allItems.Count > 0)
-            {
-                var firstItem = allItems[0];
-
-                var itemById = stockRepo.GetStockItemById(firstItem.Id);
-                var itemByCode = stockRepo.GetStockItemByCode(firstItem.ItemCode);
-
-                stockRepo.UpdateStockQuantity(firstItem.Id, 99);
-                var afterUpdate = stockRepo.GetStockItemById(firstItem.Id);
-
-                stockRepo.AdjustStockQuantity(firstItem.Id, -4);
-                var afterAdjust = stockRepo.GetStockItemById(firstItem.Id);
-
-                stockRepo.ReceiveStock(firstItem.Id, 10);
-                var afterReceive = stockRepo.GetStockItemById(firstItem.Id);
-
-                stockRepo.ConsumeStock(firstItem.Id, 3);
-                var afterConsume = stockRepo.GetStockItemById(firstItem.Id);
-            }
-
-            var nbnSuppliedItems = stockRepo.GetStockItemsBySupplyType(SupplyType.NbnSupplied);
         }
     }
 }
