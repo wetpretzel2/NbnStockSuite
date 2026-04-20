02fbb45c7b78de25af8c4dd76a41b12c630dac1a
scott dare <scott.dare@outlook.com.au>
2026-04-20 20:25:57 +1000

    Add automatic light/dark theme support
    Detect Windows theme and apply matching light/dark theme at startup. Add LightTheme.xaml and DarkTheme.xaml with color resources for backgrounds, text, borders, and DataGrid elements. Update App.xaml and MainWindow.xaml to use dynamic resources for theming. The UI now adapts to the user's system theme preference.

--- NbnStock.Windows/App.xaml.cs
+++ NbnStock.Windows/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using NbnStock.Core.Data;
+﻿using Microsoft.Win32;
+using NbnStock.Core.Data;
+using System;
 using System.Windows;
 
 namespace NbnStock.Windows
@@ -10,6 +12,41 @@ namespace NbnStock.Windows
             base.OnStartup(e);
 
             DatabaseInitialiser.Initialise();
+
+            string startupTheme = GetWindowsTheme();
+            ApplyTheme(startupTheme);
+        }
+
+        public void ApplyTheme(string themeName)
+        {
+            string themePath = themeName switch
+            {
+                "Dark" => "Themes/DarkTheme.xaml",
+                _ => "Themes/LightTheme.xaml"
+            };
+
+            var newTheme = new ResourceDictionary
+            {
+                Source = new Uri(themePath, UriKind.Relative)
+            };
+
+            Resources.MergedDictionaries.Clear();
+            Resources.MergedDictionaries.Add(newTheme);
+        }
+
+        private string GetWindowsTheme()
+        {
+            const string registryKey = @"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+            const string valueName = "AppsUseLightTheme";
+
+            object? value = Registry.CurrentUser.OpenSubKey(registryKey)?.GetValue(valueName);
+
+            if (value is int intValue)
+            {
+                return intValue == 0 ? "Dark" : "Light";
+            }
+
+            return "Light";
         }
     }
 }
