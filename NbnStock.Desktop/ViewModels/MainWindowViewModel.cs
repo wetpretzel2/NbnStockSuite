5594fa2893e53294d1f9b4b841cc474117d3d3d4
scott <scott.dare.sd@gmail.com>
2026-08-11 21:01:35 +1000

    Refactor MainWindowViewModel by removing unused properties and methods

--- NbnStock.Desktop/ViewModels/MainWindowViewModel.cs
+++ NbnStock.Desktop/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,6 @@
 using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using NbnStock.Core.Data;
-using NbnStock.Core.Repositories;
-using System.Collections.ObjectModel;
-using NbnStock.Core.Models;
 
 namespace NbnStock.Desktop.ViewModels;
 
@@ -26,14 +22,14 @@ public class MainWindowViewModel : INotifyPropertyChanged
         }
     }
     public event PropertyChangedEventHandler? PropertyChanged;
-    public ObservableCollection<StockItem> StockItems { get; } = [];
+    
     
     public MainWindowViewModel()
     {
         CurrentPage = new InventoryViewModel();
     }
     private string _statusMessage = "Ready";
-    private StockItem? _selectedStockItem;
+    
     
     private void OnPropertyChanged(
         [CallerMemberName] string? propertyName = null)
@@ -43,29 +39,7 @@ public class MainWindowViewModel : INotifyPropertyChanged
             new PropertyChangedEventArgs(propertyName));
     }
     
-    public void LoadStockItems()
-    {
-        try
-        {
-            var repository = new StockRepository();
-            var stockItems = repository.GetAllStockItems();
-
-            StockItems.Clear();
-
-            foreach (var stockItem in stockItems)
-            {
-                StockItems.Add(stockItem);
-            }
-
-            StatusMessage =
-                $"Loaded {stockItems.Count} stock items from {DatabaseInitialiser.DatabasePath}";
-        }
-        catch (Exception ex)
-        {
-            StatusMessage =
-                $"Failed to load stock items: {ex.Message}";
-        }
-    }
+    
     
     public string StatusMessage
     {
@@ -80,16 +54,5 @@ public class MainWindowViewModel : INotifyPropertyChanged
         }
     }
 
-    public StockItem? SelectedStockItem
-    {
-        get => _selectedStockItem;
-        set
-        {
-            if (_selectedStockItem == value)
-                return;
-
-            _selectedStockItem = value;
-            OnPropertyChanged();
-        }
-    }
+    
 }
